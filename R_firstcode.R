558222b7413693049ce8c6b67d1f5250e64e73c8
Francesca1400 <127427344+Francesca1400@users.noreply.github.com>
2023-03-10 17:58:13 +0100

    Update R_firstcode.R

--- R_firstcode.R
+++ R_firstcode.R
@@ -2,3 +2,20 @@
 # Let's install the raster package
 install.packages("raster")
 library("raster")
+setwd("C:/Lab/")
+l2011 <-brick("p224r63_2011_masked.grd)
+#plotting the data in a savage manner
+plot(l2011)
+
+colorRampPalette
+clb <- colorRampPalette(c("red", "orange", "yellow")) (100)
+plot(l2011, col=clb)
+
+#plotting one element
+plot(l2011[[4]], col=clb)
+
+plot(l2011$B4_sre, col=clb)
+
+nir <-l2011 [[4]]
+plot(nir, col=clb)
+
