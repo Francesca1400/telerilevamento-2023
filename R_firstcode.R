d7b0ff63cfbb5b78ff56a264e2f2cfaa2157555d
Francesca1400 <127427344+Francesca1400@users.noreply.github.com>
2023-03-24 16:25:59 +0100

    Update R_firstcode.R

--- R_firstcode.R
+++ R_firstcode.R
@@ -59,3 +59,36 @@ plot(l2011[[3]], col=clred)
 clNIR <- colorRampPalette(c("darkorchid4", "chocolate4", "darkorange")) (100)
 plot(l2011[[4]], col=clNIR)
 
+# RGB plotting
+plotRGB(l2011, r=3, g=2, b=1, stretch="Lin")
+plotRGB(l2011, r=4, g=3, b=2, stretch="Lin")
+
+# Multiframe with natural and false colours
+plotRGB(l2011, r=3, g=2, b=1, stretch="Lin")
+plotRGB(l2011, r=3, g=2, b=4, stretch="Lin")
+
+# Histogram stratching
+par(mfrow = c(2,1))
+plotRGB(l2011, 4, 3, 2, stretch="Hist")
+
+# Excersise: import the 1988 image
+l1988 <- brick("p224r63_1988_masked.grd")
+l1988
+plot(l1988)
+
+plotRGB(l1988, r=3, g=2, b=1, stretch="Lin")
+plotRGB(l1988, r=4, g=3, b=2, stretch="Lin")
+
+# Multiframe
+par(mfrow = c(2,1))
+plotRGB(l1988, 4, 3, 2, stretch="Lin")
+plotRGB(l2011, 4, 3, 2, stretch="Lin")
+
+# Esercizio, fare un multiframe 2 righe e 2 colonne, 1988 lin, 2011 lin, 1988 hist, 2011 hist
+# RGB con infrarosso in red
+par(mfrow = c(2,2))
+plotRGB(l1988, r=4, g=3, b=2, stretch="Lin")
+plotRGB(l2011, r=4, g=3, b=2, stretch="Lin")
+plotRGB(l1988, r=4, g=3, b=2, stretch="Hist")
+plotRGB(l2011, r=4, g=3, b=2, stretch="Hist")
+
