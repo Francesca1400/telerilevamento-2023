e45fcb5e2069a69da757b893251da258b4e16b94
Francesca1400 <127427344+Francesca1400@users.noreply.github.com>
2023-04-20 17:40:11 +0200

    Update R_code_classification.R

--- R_code_classification.R
+++ R_code_classification.R
@@ -5,9 +5,9 @@
 # library(RStoolbox)
 
 # setwd
-("C:/lab/") 
+setwd("C:/lab/")  
 
-# data import
+# data import with brick function
 so <- brick("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg")
 
 #plotting
@@ -16,24 +16,21 @@ plotRGB(so, 1, 2, 3, stretch="hist")
 # Classifying the solar data
 
 
-# 1. Get all the single values
+# 1) Get all the single values
 singlenr <- getValues(so)
 singlenr
 
-# 2. Classify
+# 2) Classify
 kcluster <- kmeans(singlenr, centers = 3)
 kcluster
 
-# 3. Set values to a raster on the basis of so
-soclass <- setValues(so[[1]], kcluster$cluster) # assign new values to a raster object
+# 3) Set values to a raster on the basis of so
+soclass <- setValues(so[[1]], kcluster$cluster) 
 
 cl <- colorRampPalette(c('yellow','black','red'))(100)
 plot(soclass, col=cl)
 
-# set.seed can be used for repeating the experiment in the same manner for N times
-
-# day 2 Grand Canyon
-
+# Grand Canyon data
 gc <- brick("dolansprings_oli_2013088_canyon_lrg.jpg")
 gc
 
@@ -41,27 +38,37 @@ gc
 # verde = 2
 # blu = 3
 
+# Plotting
 plotRGB(gc, r=1, g=2, b=3, stretch="lin")
-
-# change the stretch to histogram stretching
 plotRGB(gc, r=1, g=2, b=3, stretch="hist")
 
-# classification
+#Crop the imagine
+gcc <- crop (gc, drawExtent())
+plotRGB(gcc, 1, 2, 3, stretch= "lin")
 
-# 1. Get values
+# Classification
+
+# 1) Get values
 singlenr <- getValues(gc)
 singlenr
 
-# 2. Classify
+# 2) Classify
 kcluster <- kmeans(singlenr, centers = 3)
 kcluster
 
-# 3. Set values
-gcclass <- setValues(gc[[1]], kcluster$cluster) # assign new values to a raster object
+# 3) Set values
+gcclass <- setValues(gc[[1]], kcluster$cluster)
+
 
 cl <- colorRampPalette(c('yellow','black','red'))(100)
 plot(gcclass, col=cl)
 
+#Frequencies
+
 frequencies <- freq(gcclass)
+total = ncell(gcclass)
+total
 tot = 58076148
+
+#Percentages
 percentages = frequencies * 100 /  tot
