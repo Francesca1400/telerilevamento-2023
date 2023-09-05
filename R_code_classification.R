4e2adc39c7dc1abea1ef5ce78e43cd4d9c07b5ae
Francesca1400 <127427344+Francesca1400@users.noreply.github.com>
2023-09-05 20:02:48 +0200

    R_code_classification.R

--- R_code_classification.R
+++ R_code_classification.R
@@ -1,26 +1,34 @@
-#Classification of remote sensing data via RStoolbox
+# Classification of remote sensing data via RStoolbox
 
-#Installing devtools
-# install.packages("RStoolbox")
-# library(RStoolbox)
+# Installing devtools
+install.packages("raster")
+install.packages("devtools")
+install.packages("RStoolbox")
 
-# setwd
-setwd("C:/lab/")  
+# Recall the packages with library
+library(devtools)
+library(raster)
+library(RStoolbox)
 
-# data import with brick function
+# Set working directory
+setwd("C:/lab/")  #Windows
+
+# Data import with brick function
 so <- brick("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg")
 
-#plotting
+# Plotting with two different stretch
 plotRGB(so, 1, 2, 3, stretch="lin")
 plotRGB(so, 1, 2, 3, stretch="hist")
-# Classifying the solar data
 
+# Classifying the solar data
 
 # 1) Get all the single values
 singlenr <- getValues(so)
 singlenr
 
 # 2) Classify
+# "kmeans" clustering used unsupervised machine learning algorithm 
+# for partitioning a given data set into a set of k groups
 kcluster <- kmeans(singlenr, centers = 3)
 kcluster
 
@@ -30,20 +38,22 @@ soclass <- setValues(so[[1]], kcluster$cluster)
 cl <- colorRampPalette(c('yellow','black','red'))(100)
 plot(soclass, col=cl)
 
+# -------------------------
 # Grand Canyon data
+
 gc <- brick("dolansprings_oli_2013088_canyon_lrg.jpg")
 gc
 
-# rosso = 1
-# verde = 2
-# blu = 3
+# red = 1
+# green = 2
+# blue = 3
 
-# Plotting
+# Plotting RGB
 plotRGB(gc, r=1, g=2, b=3, stretch="lin")
 plotRGB(gc, r=1, g=2, b=3, stretch="hist")
 
-#Crop the imagine
-gcc <- crop (gc, drawExtent())
+# Crop the imagine
+gcc <- crop (gc, drawExtent()) # "crop" returns a geographic subset of an object
 plotRGB(gcc, 1, 2, 3, stretch= "lin")
 
 # Classification
@@ -59,16 +69,14 @@ kcluster
 # 3) Set values
 gcclass <- setValues(gc[[1]], kcluster$cluster)
 
-
 cl <- colorRampPalette(c('yellow','black','red'))(100)
 plot(gcclass, col=cl)
 
-#Frequencies
-
+# Let's calculate the frequency
 frequencies <- freq(gcclass)
 total = ncell(gcclass)
 total
 tot = 58076148
 
-#Percentages
+# Let's calculate the percentages
 percentages = frequencies * 100 /  tot
