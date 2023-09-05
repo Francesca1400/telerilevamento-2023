9fac062b2cfd0eea7ecadb8fd01cff20d2e8879a
Francesca1400 <127427344+Francesca1400@users.noreply.github.com>
2023-09-05 21:11:11 +0200

    R_code_Land_cover.R

--- R_code_Land_cover.R
+++ R_code_Land_cover.R
@@ -1,101 +1,107 @@
+# Classification for land cover
+
+# Install packages and recall with library ggplot2 for graphs
 install.packages(ggplot2)
-library(ggplot2)   #for ggplot graphs
+library(ggplot2)  
 library(raster)
+
+# Set working directory
 setwd("C:/lab/")
 
+# Import the necessary files
 defor1 <- brick("defor1_.png")
 defor2 <- brick("defor2_.png")
 
-#PLotting RGB
+# Plotting RGB
+# NIR 1 , RED2 , GREEN 3
 par(mfrow=c(1,2))
 plotRGB(defor1, r=1, g=2, b=3, stretch="lin")
 plotRGB(defor2, r=1, g=2, b=3, stretch="lin")
 
-#Classification defor1
-#Get all the single values
+# Classification defor1
+# Get all the single values
 singlenr1 <- getValues(defor1)
 singlenr1
 
-#Classify with kmeans
+# Classify with kmeans
 kcluster1 <- kmeans(singlenr1, centers = 2)
 kcluster1
 
-#Recreating an image
+# Recreating an image
 defor1class <- setValues(defor1[[1]], kcluster1$cluster)
 
-#Plotting
+# Plotting
 plot(defor1class)
-#Class1: forest
-#Class2: bare soil
 
-#Classification defor2
-#1 step
+# Class1: forest
+# Class2: bare soil
+
+# Classification defor2
+
+# 1 step: Get all the single values
 singlenr2 <- getValues(defor2)
 singlenr2
 
-#2 step
+# 2 step: Classify
 kcluster2 <- kmeans(singlenr2, centers = 2)
 kcluster2
 
-#3 step 
+# 3 step: Set the values to a raster
 defor2class <- setValues(defor2 [[1]], kcluster2$cluster)
 
-#Plotting
+# Plotting
 plot(defor2class)
 
-#Class1: forest
-#Class2 : agricolture
+# Class1: forest
+# Class2 : agricolture
 
-#For multiframes with ggplot2
+# For multiframes with ggplot2 use patchwork and then recall it with the library
 install.packages("patchwork")
 library(patchwork)
 
-#Class percentages defor1
+# Now let's calculate the frequency and the percentage
 
 frequencies1 <- freq(defor1class)
 tot1 = ncell(defor1class)
-tot1   #tot1=341292
+tot1   # Tot 1 =341292
 percentages1 = frequencies1 * 100 /  tot1
-percentages1  #Class1 = 89.74632%  #Class2 = 10.25368%
+percentages1  
 
-#Class percentages defor2
+# Class1 = 89.74632%  
+# Class2 = 10.25368%
+
+# Class percentages defor2
 frequencies2 <- freq(defor2class)
 tot2 = ncell(defor2class)
-tot2  #Tot 2 = 342726
-
+tot2  # Tot 2 = 342726
 percentages2 = frequencies2 * 100 /  tot2
-percentages2  #Class1 = 47.931  #Class2 = 52.069
+percentages2  
 
-#Build a dataframe
+# Class1 = 47.931  
+# Class2 = 52.069
 
+# Build a dataframe
 cover <- c("Forest", "Bare soil ")
 percent1992 <- c(89.74, 10.25)
 percent2006 <- c(47.93, 52,09)
 
-#creating dataframe
-#final tabl
-
+# Creating dataframe
 percent_1992 <- c(89.83, 10.16)
 percent_2006 <- c(52.06, 47.93)
-
 percentages <- data.frame(cover, percent_1992, percent_2006)
 percentages
 
-#Plotting with ggplot2
-#Plotting
+# Plotting with ggplot2
+ggplot(percentages, aes(x=cover, y=percent_1992, color=cover)) + geom_bar(stat="identity", fill="white") # 1992
+ggplot(percentages, aes(x=cover, y=percent_2006, color=cover)) + geom_bar(stat="identity", fill="white") # 2006
 
-#1992
-ggplot(percentages, aes(x=cover, y=percent_1992, color=cover)) + geom_bar(stat="identity", fill="white")
-#2006
-ggplot(percentages, aes(x=cover, y=percent_2006, color=cover)) + geom_bar(stat="identity", fill="white")
-
-#Petchwork of the two graphics
+# Petchwork of the two graphics
 p1 <- ggplot(percentages, aes(x=cover, y=percent_1992, color=cover)) + geom_bar(stat="identity", fill="white")
 p2 <- ggplot(percentages, aes(x=cover, y=percent_2006, color=cover)) + geom_bar(stat="identity", fill="white")
 p1+p2
 
-#put the same percentages limits
-#use ylim function
+# Put the same percentages limits
+# Use ylim function, ìthis function is used to set the lower and upper limit on the y-axis and x-axis respectively.
 p1 <- ggplot(percentages, aes(x=cover, y=percent_1992, color=cover)) + geom_bar(stat="identity", fill="white") + ylim(c(0,100))
 p2 <- ggplot(percentages, aes(x=cover, y=percent_2006, color=cover)) + geom_bar(stat="identity", fill="white") + ylim(c(0,100))
 p1+p2
