ae79aa898e9a9a21197bcb453fc892bbbf46f39b
Francesca1400 <127427344+Francesca1400@users.noreply.github.com>
2023-09-05 19:30:12 +0200

    R_code_multivariate_analysis.R

--- R_code_multivariate_analysis.R
+++ R_code_multivariate_analysis.R
@@ -1,53 +1,77 @@
-setwd("C:/lab/")
+# Multivariate analysis
+
+# Set workig directory
+setwd("C:/lab/") #Windows
+
+# Retrieve the packages I need from the library
 library(raster)
 library(ggplot2)
 library(viridis)
 library(patchwork)
+
+# Import images
 sen <- brick("sentinel.png")
 sen2 <- stack(sen[[1]], sen[[2]], sen[[3]])
+
+# Plotting
 plot(sen2)
-pairs(sen2) # Vediamo la correlazione tra le bande
+pairs(sen2) # To see the correlation between the various bands
+
 # PCA (Principal Component Analysis)
-sample <- sampleRandom(sen2, 10000) # abbiamo creato un campione
+sample <- sampleRandom(sen2, 10000) 
 pca <- prcomp(sample)
-summary(pca) # ci dà le informazioni, deviazione standar e quantità di variabilità spiegata da ogni componente
-#usando la prima componenete abbiamo già il 68% di variabilità spiegata
-#le prime due spiegano quasi tutta la variabilità, il 99.6%
-plot(pca) #vediamo le variabilità delle tre componenti
-#dobbiamo prevedere ora il valore degli altri pixel
-pci <- predict(sen2, pca, index=c(1:3)) # index rappresenta quante componenti vogliamo in uscita
+summary(pca) 
+# Gives us the information, standard deviation and amount of variability explained by each component
+# using the first component we already have 68% of variability explained while,
+# the first two explain almost all the variability, about 99.6%
+
+# Let's look at the variability between the components
+plot(pca) 
+
+# We now predict the value of the other pixels
+pci <- predict(sen2, pca, index=c(1:3)) # index represents how many components we want in output
 plot(pci)
-# usiamo ggplot per dei grafici migliori
-pcid1 <- as.data.frame(pci[[1]], xy=T) # ggplot vuole dati tabellari e non raster
-pci # vediamo come si chiama la prima componente per il fill
+
+# Let's use ggplot2 now
+pcid1 <- as.data.frame(pci[[1]], xy=T) # ggplot wants tabular data and not raster
+pci
+
 ggplot() +
   geom_raster(pcid1, mapping = aes(x=x, y=y, fill=PC1)) +
   scale_fill_viridis()
+
 pcid3 <- as.data.frame(pci[[3]], xy=T) 
 ggplot() +
   geom_raster(pcid3, mapping = aes(x=x, y=y, fill=PC3)) +
   scale_fill_viridis()
-# adesso con patchwork li mettiamo uno accanto all'altro
+
+# We use patchwork to put them side by side
 plot1 <- ggplot() +
   geom_raster(pcid1, mapping = aes(x=x, y=y, fill=PC1)) +
   scale_fill_viridis() +
   ggtitle("Principal Component 1")
+
 plot3 <- ggplot() +
   geom_raster(pcid3, mapping = aes(x=x, y=y, fill=PC3)) +
   scale_fill_viridis() +
   ggtitle("Principal Component 3")
+
 plot1 + plot3
-# calcoliamo la deviazione standard di PC1 con la funzione focal
+
+# We calculate the standard deviation of PC1 with the "focal" function
 sd3 <- focal(pci[[1]], matrix(1/9, 3, 3), fun=sd)
-sd3 # names = layer
+sd3
 sd3d <- as.data.frame(sd3, xy=T)
-#patchwork per PC1 e la sua deviazione standard
+
+# I make a patchwork for PC1 and its standard deviation
 plotsd <- ggplot() +
   geom_raster(sd3d, mapping = aes(x=x, y=y, fill=layer)) +
   scale_fill_viridis(option="inferno") +
   ggtitle("Standard Deviation of PC1 (3x3)")
+
 plot1 <- ggplot() +
   geom_raster(pcid1, mapping = aes(x=x, y=y, fill=PC1)) +
   scale_fill_viridis(option="inferno") +
   ggtitle("Principal Component 1")
+
 plot1 + plotsd
