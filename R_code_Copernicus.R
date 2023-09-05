74b9df3e10ff4e0522469835da4cc0fb79ac9b21
Francesca1400 <127427344+Francesca1400@users.noreply.github.com>
2023-09-05 21:18:13 +0200

    R_code_Copernicus.R

--- R_code_Copernicus.R
+++ R_code_Copernicus.R
@@ -1,37 +1,40 @@
 #Copernicus data
 
+# Install the packages I need and call others with the library
 install.packages("ncdf4")
 library(ncdf4)
 library(raster)
 library(ggplot2)
 library(viridis)
 
-#setwd("C:/lab/")
-#setwd("/Users/name/Desktop/lab/") 
+# Set working directory
+setwd("C:/lab/") 
 
-#dataset: c_gls_SSM1km_202305090000_CEURO_S1CSAR_V1.2.1.nc
+# Dataset: c_gls_SSM1km_202305090000_CEURO_S1CSAR_V1.2.1.nc
 
 sc <- raster("c_gls_SSM1km_202305090000_CEURO_S1CSAR_V1.2.1.nc")
+
+#Plotting with R function
 plot(sc)
 
-#with the raster package -> RasterLayer
-#with the terra package -> SpatRaster: it maintains the uncertainty data, so: 2 layers
+# With the raster package -> RasterLayer
+# With the terra package -> SpatRaster: it maintains the uncertainty data, so: 2 layers
 
-scd <- as.data.frame(sc, xy=T)
-
+# Plotting
 ggplot() +
 geom_raster(scd, mapping=aes(x=x, y=y, fill=Surface.Soil.Moisture)) +
 ggtitle("Soil Moisture from Copernicus")
 
-#Cropping an image
+# Cropping an image
 ext <- c(23, 30, 62, 68)
 sc.crop <- crop(sc, ext)
 
-#Exercise: plot via ggplot the cropped image
+# Exercise: plot via ggplot the cropped image
 sc.crop.d <- as.data.frame(sc.crop, xy=T)
 head(sc.crop.d)
 names(sc.crop.d)
 
+# Plotting with cividis
 ggplot() +
 geom_raster(sc.crop.d, mapping=aes(x=x, y=y, fill=Surface.Soil.Moisture)) +
 ggtitle("Cropped Soil Moisture from Copernicus") +
