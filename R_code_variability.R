b3eca2f164c21f87986dfab557bac164e07643f3
Francesca1400 <127427344+Francesca1400@users.noreply.github.com>
2023-09-05 18:26:17 +0200

    R_code_variability.R

--- R_code_variability.R
+++ R_code_variability.R
@@ -2,32 +2,33 @@
 library (raster)
 library(ggplot2)
 
+#Set the working directory
 setwd("C:/Lab/") # Windows
 
 # Import image
 sen <- brick("sentinel.png")
 plot(sen) 
 
-#Plotting RGB
+# Plotting RGB
 RGB(sen, 1, 2, 3, stretch ="lin")
 
 plonir <- sen[[1]]
 sd3 <- focal(nir, matrix(1/9, 3, 3), fun=sd)
 
-#Plotting
+# Plotting
 plot(sd3) 
 
-#Now plotting with ggplot2
+# Now plotting with ggplot2
+# Ggploot2 is a R package dedicated to data visualization
 sd3d <- as.data.frame(sd3, xy=TRUE)
-
 ggplot()+ 
-geom_raster(sd3d, mapping = aes(x=x, y=y, fill=layer))
+  geom_raster(sd3d, mapping = aes(x=x, y=y, fill=layer))
 
-#Install viridis packages
+# Install viridis packages
 install.packages("viridis")
 library(viridis)
 
-#Plotting with ggplot2 e viridis
+# Plotting with ggplot2 and viridis
 ggplot()+
   geom_raster(sd3d, mapping = aes(x=x, y=y, fill=layer))+
   scale_fill_viridis()
@@ -36,39 +37,33 @@ ggplot()+
   geom_raster(sd3d, mapping = aes(x=x, y=y, fill=layer))+
   scale_fill_viridis(option="magma")
   
-  
-#Add the title
+# Now add the title
  ggplot()+
   geom_raster(sd3d, mapping = aes(x=x, y=y, fill=layer))+
   scale_fill_viridis(option="magma")+
   ggtitle("Standard deviation via the magma colour scale")
   
-
-#Multiframe
-
-#Viridis
+# Use patchwork to merge different ggplots within the same plot
+# Viridis
 p1 <- ggplot()+
    geom_raster(sd3d, mapping = aes(x=x, y=y, fill=layer))+
    scale_fill_viridis()
    ggtitle("Standard deviation via the viridis colour scale")
   
-#Inferno
-p2<- ggplot()+
+# Inferno
+p2 <- ggplot()+
    geom_raster(sd3d, mapping = aes(x=x, y=y, fill=layer))+
    scale_fill_viridis(option="inferno")+
    ggtitle("Standard deviation via the inferno colour scale")
-   
 p1+ p2
 
-#Excercise: plot the original image (nir) and its standard deviation
-#Dataframe
+# xcercise: plot the original image (nir) and its standard deviation
 nird<- as.data.frame(nir, xy=T)
-
-p3<- ggplot()+
+p3 <- ggplot()+
   geom_raster(nird, mapping = aes(x=x, y=y, fill=sentinel_1))+
   scale_fill_viridis(option="cividis")+
   ggtitle("NIR deviation via the cividis colour scale")
-
+p3
 p3+p1
 
   
