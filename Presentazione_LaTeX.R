68a82e02df09df9b45b4a067026abca5b51829ea
Francesca1400 <127427344+Francesca1400@users.noreply.github.com>
2023-09-11 23:51:21 +0200

    Create Presentazione_LaTeX.R

---
+++ Presentazione_LaTeX.R
@@ -0,0 +1,52 @@
+% My LaTeX code for the related presentation
+% in the ecological remote sensing exam of Prof.D. Rocchini
+
+\documentclass{beamer}
+\usepackage{graphicx} % Required for inserting images
+\usepackage{color}
+\usepackage{natbib}
+\usepackage{hyperref}
+
+\usetheme{JuanLesPins}
+\usecolortheme{spruce}
+
+\author{Francesca Cipolletta}
+\title{Esame telerilevamento geoecologico}
+\date{15 Settembre 2023}
+
+\begin{document}
+
+\maketitle
+
+\AtBeginSection[]
+{
+\begin{frame}
+\frametititle{}
+\tableofcontents[currentsection]
+\end{frame}
+}
+
+\section{Introduzione}
+
+\begin{frame}{Sommario}
+Il progetto si suddivide nelle seguenti \textbf{sezioni}:
+\begin{itemize}
+    \item Obiettivo del progetto
+    \item Importazione e visualizzazione delle immagini 
+    \item Calcolo DVI e NDVI
+    \item Classificazione e land cover
+\end{itemize}
+\end{frame}
+
+\begin{frame}{Obiettivo del progetto}
+Il progetto mira ad analizzare lo stato di salute della \textbf{vegetazione} in \textbf{Sicilia}, precisamente nell'area di Belmonte in provincia di Catania.
+    
+\end{frame}
+
+
+\section{Materiali e metodi}
+\section{Risultati}
+\section{Conclusione}
+
+
+\end{document}
